cc384d83afda680c58f365246b719da5edf7a238
Battlecator <36232657+Battlecator@users.noreply.github.com>
2020-05-13 06:48:28 +0300

    logic change

--- build.sh
+++ build.sh
@@ -15,31 +15,34 @@ steam_dir="/home/a3/steamcmd"
 modules_dir="/home/a3/serverfiles/modules"
 workshop_dir="/home/a3/Steam/steamapps/workshop/content"
 
-mv -t ${repo_dir} ${modules_dir}/* ${modules_dir}/.a3s
+#mv -t ${repo_dir} ${modules_dir}/* ${modules_dir}/.a3s
 
 while IFS=, read -r name id cmd
-do	
-	if [ ${cmd:-empty} == empty ]
+do
+	if [ ${cmd} == r ]
 	then
-		if [ ${cmd} == u ] || [ ${cmd} == r ]
+#		rm -r ${repo_dir}/${name}
+		echo rm ${name}
+	elif [ ${id} -gt 30000 ]
+	then
+		if [ ${cmd} == u ]
 		then
-			rm -r ${repo_dir}/${name}
+#			rm -r ${repo_dir}/${name}
+			echo workshop remove ${name}
 		fi
-	
-		if [ ${cmd} == u ] || [ ${cmd} == d ]
-		then
-			/bin/bash ${steam_dir}/steamcmd.sh +login "${steamuser}" "${steampass}" +workshop_download_item "${app}" "${id}" +quit
-			mv ${workshop_dir}/${app}/${id} ${repo_dir}/${name}
-			find ${repo_dir}/${name} -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
-		fi
+		
+#		/bin/bash ${steam_dir}/steamcmd.sh +login "${steamuser}" "${steampass}" +workshop_download_item "${app}" "${id}" +quit
+#		mv ${workshop_dir}/${app}/${id} ${repo_dir}/${name}
+#		find ${repo_dir}/${name} -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
+		echo workshop add ${name}
 	fi
 done < modlist.csv
 
-cd ${a3s_dir}
-java -jar ${a3s_dir}/ArmA3Sync.jar -BUILD "${repo_name}"
+#cd ${a3s_dir}
+#java -jar ${a3s_dir}/ArmA3Sync.jar -BUILD "${repo_name}"
 
-mv -t ${modules_dir} ${repo_dir}/* ${repo_dir}/.a3s
+#mv -t ${modules_dir} ${repo_dir}/* ${repo_dir}/.a3s
 
-/bin/bash /home/a3/a3s r
+#/bin/bash /home/a3/a3s r
 
 exit
